51845ab30eb3e54c2197351a08a83e30ec5862e9
PegasUsOnce <rosliakovaov@gmail.com>
2022-05-10 15:37:56 +1000

    1201 accepted

--- 1201.cpp
+++ 1201.cpp
@@ -38,31 +38,28 @@ int get_day_of_week(const int day, const int month, const int year) {
 }
 
 
-void print_day_of_week(const int day_of_week, const bool skipFirst, const int firstNum,
-                       const int maxNum, const int selectedNum, const bool selectedInOneDigitWeek) {
-    printf("%s ", day_of_week_name[day_of_week - 1]);
+void print_day_of_week(const int day_of_week, const bool skipFirst, const int firstNum, const int maxNum, const int selectedNum) {
+
+    /*
+    mon........4...11...18...25
+    tue........5...12...19...26
+    wed........6...13...20...27
+    thu........7...14...21...28
+    fri...1....8...15...22...29
+    sat...2....9..[16]..23...30
+    sun...3...10...17...24...31
+    */
+
+    printf("%s", day_of_week_name[day_of_week - 1]);
     if (skipFirst) {
-        printf("    "); // 4 spaces
+        printf("     "); // 5 spaces
     }
 
-    for (int currentNum = firstNum; currentNum <= maxNum; currentNum = currentNum + 7){
-        bool needSpecialSpace = currentNum != selectedNum && (currentNum - day_of_week + 1) < 10 && (currentNum - day_of_week + 7) >= 10 && (currentNum < 10);
-        if (needSpecialSpace)
-            printf(" ");
-
+    for (int currentNum = firstNum; currentNum <= maxNum; currentNum = currentNum + 7) {
         if (currentNum == selectedNum)
-            if (selectedNum >= 10)
-                printf(" [%d]", currentNum);
-            else
-            {
-                if (selectedInOneDigitWeek)
-                    printf("[ %d]", currentNum);
-                else
-                    printf(" [ %d]", currentNum);
-
-            }
+            printf(" [%2d]", currentNum);
         else
-            printf("  %d ", currentNum);
+            printf("  %2d ", currentNum);
     }
 
     printf("\n");
@@ -78,23 +75,12 @@ int main() {
 
     int daysInMonth = days_in_month[month - 1] + ((month == 2 && is_leap_year(year)) ? 1 : 0);
     int firstMonday = firstDayOfWeek == 1 ? 1 : 9 - firstDayOfWeek;
-    bool selectedInOneDigitWeek = (firstMonday > day) || (day < 10 && (firstMonday + 7 <= 10 && firstMonday + 7 > day));
 
     for (int i = 1; i < 8; i++) {
         int startNum = firstDayOfWeek <= i ? 1 + (i - firstDayOfWeek) : 8 - firstDayOfWeek + i;
         bool skipFirst = i < firstDayOfWeek;
-        print_day_of_week(i, skipFirst, startNum, daysInMonth, day, selectedInOneDigitWeek);
+        print_day_of_week(i, skipFirst, startNum, daysInMonth, day);
     }
-
-/*
-mon........4...11...18...25
-tue........5...12...19...26
-wed........6...13...20...27
-thu........7...14...21...28
-fri...1....8...15...22...29
-sat...2....9..[16]..23...30
-sun...3...10...17...24...31
-*/
 
     return 0;
 }
